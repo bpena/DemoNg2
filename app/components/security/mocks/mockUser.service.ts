d53484dd173da0e2ee4f7437ed18da1a9deb6123
bpena <bernardo.pena.ramos@gmail.com>
2016-06-19 17:05:52 -0400

    ajuste de usuarios mocks

--- app/components/security/mocks/mockUser.service.ts
+++ app/components/security/mocks/mockUser.service.ts
@@ -5,5 +5,5 @@ export var USERS: User[] = [
     {id: 1, firstname: 'admin',     lastname: 'admin',      username: 'admin',      password: 'admin123',       image: ''},
     {id: 2, firstname: 'Bernardo',  lastname: 'Peña',       username: 'bpena',      password: 'nano123',        image: ''},
     {id: 3, firstname: 'Walter',    lastname: 'Popovich',   username: 'wpopovich',  password: 'wpopovich',      image: ''},
-    {id: 4, firstname: 'Ronye',     lastname: 'Vernaes',    username: 'ramaush',    password: 'erlocomarico123',image: ''}
+    {id: 4, firstname: 'Ronye',     lastname: 'Vernaes',    username: 'remnahush',    password: 'ronye123',     image: ''}
 ];
